afcc3acf7d212049dec1edc048cd36c3bd95d2cb
alicezangliger <55668804+alicezangliger@users.noreply.github.com>
2019-11-18 23:47:12 +0300

    Reformatting code

--- ChessBoard/ChessBoard/Cell.cs
+++ ChessBoard/ChessBoard/Cell.cs
@@ -1,93 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ChessBoard
 {
     class Cell
     {
-        protected char collumn;
-        protected int row;
-        public Cell(char collumn, int row)
+        public char column { get; set; }
+        public int row { get; set; }
+        public Cell(char column, int row)
         {
 
-            if ((collumn < 65 || collumn > 104) || (collumn < 97 && collumn > 72) || (row < 1 || row > 8))
+            if ((column < 65 || column > 104) || (column < 97 && column > 72) || (row < 1 || row > 8))
             {
                 throw new Exception("Incorrect direction,please try again");
             }
             else
             {
-                this.collumn = collumn;
+                this.column = column;
                 this.row = row;
             }           
 
-        }
-        public char GetCollumn()
-        {
-            return collumn;
-        }
-        public int GetRow()
-        {
-            return row;
-        }
+        } 
 
-        int collumnToInt()
+        int columnToInt()
         {
-            if(collumn>=65 && collumn<=72)
+            if(column>=65 && column<=72)
             {
-                return collumn - 64;
+                return column - 64;
             }
             else
             {
-                return collumn - 96;
+                return column - 96;
             }
         }
 
-        public bool isBlack()
+        public bool IsBlack()
         {
-            int collumnInt = collumnToInt();
-            int sum = collumnInt + row;
-            if (sum % 2 == 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            int columnInt = columnToInt();
+            int sum = columnInt + row;
+            return sum % 2 == 0;
         }
 
-        public bool isWhite()
+        public bool IsWhite()
         {
-            return !isBlack();
+            return !IsBlack();
         }
 
-       public bool vertically(Cell cell)
+       public bool Vertically(Cell cell)
         {
-            int collmnInt = collumnToInt();
-            cell.collumnToInt();
-            if (collmnInt == cell.collumnToInt())
-            {
-                return true;
-            }
-            return false;
+            int colmnInt = columnToInt();
+            cell.columnToInt();
+            return colmnInt == cell.columnToInt();
         }
-        public bool horizontally(Cell cell)
-        {            
-            if (row == cell.row)
-            {
-                return true;
-            }
-            return false;
-        }
-        public bool diagonally(Cell cell)
+        public bool Horizontally(Cell cell)
         {
-            int colInt = collumnToInt();
-            if (Math.Abs(colInt - cell.collumnToInt()) == Math.Abs(row - cell.row))
-            {
-                return true;
-            }
-            return false;
+            return row == cell.row;
+        }
+        public bool Diagonally(Cell cell)
+        {
+            int colInt = columnToInt();
+            return Math.Abs(colInt - cell.columnToInt()) == Math.Abs(row - cell.row);
         }
 
     }
--- ChessBoard/ChessBoard/Program.cs
+++ ChessBoard/ChessBoard/Program.cs
@@ -10,18 +10,18 @@ namespace ChessBoard
             {
                 
                 Cell cell1 = Helper.GetCell();
-                Console.WriteLine("First cell: {0}{1}",cell1.GetCollumn(),cell1.GetRow());
-                Console.WriteLine("Color is black?: {0}", cell1.isBlack());
-                Console.WriteLine("Color is white?: {0}", cell1.isWhite());
+                Console.WriteLine("First cell: {0}{1}",cell1.column,cell1.row);
+                Console.WriteLine("Color is black?: {0}", cell1.IsBlack());
+                Console.WriteLine("Color is white?: {0}", cell1.IsWhite());
                 Console.WriteLine("********************");
                 Cell cell2 = Helper.GetCell();
-                Console.WriteLine("Second cell: {0}{1}", cell2.GetCollumn(), cell2.GetRow());
-                Console.WriteLine("Color is black?: {0}", cell2.isBlack());
-                Console.WriteLine("Color is white?: {0}", cell2.isWhite());
+                Console.WriteLine("Second cell: {0}{1}", cell2.column, cell2.row);
+                Console.WriteLine("Color is black?: {0}", cell2.IsBlack());
+                Console.WriteLine("Color is white?: {0}", cell2.IsWhite());
                 Console.WriteLine("********************");
-                Console.WriteLine("They are vertical?: {0}", cell1.vertically(cell2));
-                Console.WriteLine("They are horizontal?: {0}", cell1.horizontally(cell2));
-                Console.WriteLine("They are diagonal?: {0}", cell1.diagonally(cell2));
+                Console.WriteLine("They are vertical?: {0}", cell1.Vertically(cell2));
+                Console.WriteLine("They are horizontal?: {0}", cell1.Horizontally(cell2));
+                Console.WriteLine("They are diagonal?: {0}", cell1.Diagonally(cell2));
 
             }
             catch (Exception ex)
